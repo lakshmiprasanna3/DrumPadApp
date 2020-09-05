6f0ecbdb70ddf92ee4ca84a0c36474ea7c6a3215
lakshmiprasanna3 <prasannakalahastri@gmail.com>
2020-09-05 18:04:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/drumpadapp/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.drumpadapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer m1,m2,m3,m4,m5,m6,m7,m8;
+    ImageButton b1,b2,b3,b4,b5,b6,b7,b8;
+
+    @Override
+    public void onBackPressed() {
+        finish();
+        System.exit(0);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = findViewById(R.id.imageButton1);
+        b2 = findViewById(R.id.imageButton2);
+        b3 = findViewById(R.id.imageButton3);
+        b4 = findViewById(R.id.imageButton4);
+        b5 = findViewById(R.id.imageButton5);
+        b6 = findViewById(R.id.imageButton6);
+        b7 = findViewById(R.id.imageButton7);
+        b8 = findViewById(R.id.imageButton8);
+
+
+        m1 = MediaPlayer.create(this,R.raw.one);
+        m2 = MediaPlayer.create(this,R.raw.two);
+        m3 = MediaPlayer.create(this,R.raw.three);
+        m4 = MediaPlayer.create(this,R.raw.four);
+        m5 = MediaPlayer.create(this,R.raw.fv);
+        m6 = MediaPlayer.create(this,R.raw.sixth);
+        m7 = MediaPlayer.create(this,R.raw.seventh);
+        m8 = MediaPlayer.create(this,R.raw.eighth);
+
+
+//        b1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                m1.start();
+//            }
+//        });
+//
+//        b2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                m2.start();
+//            }
+//        });
+//
+//        b3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                m3.start();
+//            }
+//        });
+
+
+
+
+    }
+
+
+    public void play(View view) {
+
+        switch (view.getId()){
+
+            case R.id.imageButton1:{
+                m1.start();
+                break;
+            }
+            case R.id.imageButton2:{
+                m2.start();
+                break;
+            }
+            case R.id.imageButton3:{
+                m3.start();
+                break;
+            }
+            case R.id.imageButton4:{
+                m4.start();
+                break;
+            }
+            case R.id.imageButton5:{
+                m5.start();
+                break;
+            }
+            case R.id.imageButton6:{
+                m6.start();
+                break;
+            }
+            case R.id.imageButton7:{
+                m7.start();
+                break;
+            }
+            case R.id.imageButton8:{
+                m8.start();
+                break;
+            }
+        }
+
+
+    }
+}
